1ede6fedd026ab69dda255d74f85dddaeb703fe9
Reinaldia <ghazi.ghifari25@gmail.com>
2024-02-27 08:10:01 +0700

    Configured the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DashboardView from '../views/DashboardView.vue'
+import AddView from '../views/AddView.vue'
+import CheckoutView from '../views/CheckoutView.vue'
+import InvoiceView from '../views/InvoiceView.vue'
+import ReportView from '../views/ReportView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'dashboard',
+      component: DashboardView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/add',
+      name: 'add',
+      component: AddView
+    },
+    {
+      path: '/checkout',
+      name: 'checkout',
+      component: CheckoutView
+    },
+    {
+      path: '/invoice',
+      name: 'invoice',
+      component: InvoiceView
+    },
+    {
+      path: '/report',
+      name: 'report',
+      component: ReportView
     }
   ]
 })
