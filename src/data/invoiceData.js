f8671f791d04778939f182f0da674390361599b7
zareladitya <azarelbabs@gmail.com>
2024-03-04 10:21:23 +0700

    Modified Invoice Data

---
+++ src/data/invoiceData.js
@@ -0,0 +1,57 @@
+import { ref } from 'vue'
+
+export const data = ref([
+  {
+    nama: 'Teddy Lazuardi',
+    pembelian: 'Bertemu Pangeran',
+    jadwal: '15/03/2024 14.30',
+    telp: '0894732894',
+    email: 'TeddyLazuardy@gmail.com',
+    pembayaran: '216758921'
+  },
+  {
+    nama: 'Teddy Lazuardi',
+    pembelian: 'Bundling tiket masuk keraton',
+    jadwal: '15/03/2024 14.30',
+    telp: '0894732894',
+    email: 'TeddyLazuardy@gmail.com'
+  },
+  {
+    nama: 'Teddy Lazuardi',
+    pembelian: 'Event Bulanan',
+    jadwal: '15/03/2024 14.29',
+    telp: '0894732894',
+    email: 'TeddyLazuardy@gmail.com'
+  },
+  {
+    nama: 'Teddy Lazuardi',
+    pembelian: 'Event Mingguan',
+    jadwal: '15/03/2024 14.29',
+    telp: '0894732894',
+    email: 'TeddyLazuardy@gmail.com'
+  },
+  {
+    nama: 'Teddy Lazuardi',
+    pembelian: 'Bertemu Pangeran',
+    jadwal: '15/03/2024 14.29',
+    telp: '0894732894',
+    email: 'TeddyLazuardy@gmail.com'
+  },
+  {
+    nama: 'Teddy Lazuardi',
+    pembelian: 'Bertemu Pangeran',
+    jadwal: '15/03/2024 14.29',
+    telp: '0894732894',
+    email: 'TeddyLazuardy@gmail.com'
+  }
+])
+
+export const searchQuery = ref('');
+export const selectedItem = ref(null);
+
+export const formatDate = (dateTime) => {
+  const parts = dateTime.split(' ');
+  return [parts[0], parts[1]];
+};
+
+export const detailPopup = ref(null);
--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -1,62 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import InvoiceDetail from '@/components/InvoiceDetail.vue'
-
-const data = ref([
-  {
-    nama: 'Teddy Lazuardi',
-    pembelian: 'Bertemu Pangeran',
-    jadwal: '15/03/2024 14.30',
-    telp: '0894732894',
-    email: 'TeddyLazuardy@gmail.com',
-    pembayaran: '216758921'
-  },
-  {
-    nama: 'Teddy Lazuardi',
-    pembelian: 'Bundling tiket masuk keraton',
-    jadwal: '15/03/2024 14.30',
-    telp: '0894732894',
-    email: 'TeddyLazuardy@gmail.com'
-  },
-  {
-    nama: 'Teddy Lazuardi',
-    pembelian: 'Event Bulanan',
-    jadwal: '15/03/2024 14.29',
-    telp: '0894732894',
-    email: 'TeddyLazuardy@gmail.com'
-  },
-  {
-    nama: 'Teddy Lazuardi',
-    pembelian: 'Event Mingguan',
-    jadwal: '15/03/2024 14.29',
-    telp: '0894732894',
-    email: 'TeddyLazuardy@gmail.com'
-  },
-  {
-    nama: 'Teddy Lazuardi',
-    pembelian: 'Bertemu Pangeran',
-    jadwal: '15/03/2024 14.29',
-    telp: '0894732894',
-    email: 'TeddyLazuardy@gmail.com'
-  },
-  {
-    nama: 'Teddy Lazuardi',
-    pembelian: 'Bertemu Pangeran',
-    jadwal: '15/03/2024 14.29',
-    telp: '0894732894',
-    email: 'TeddyLazuardy@gmail.com'
-  }
-])
-
-const searchQuery = ref('')
-const selectedItem = ref(null)
-
-const formatDate = (dateTime) => {
-  const parts = dateTime.split(' ')
-  return [parts[0], parts[1]]
-}
-
-const detailPopup = ref(null)
+import { data, searchQuery, selectedItem, formatDate, detailPopup } from "@/data/invoiceData"
 
 const showDetail = (item) => {
   selectedItem.value = item
